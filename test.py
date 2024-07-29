31b6d6b824c5837bccefc3c050724c108d1607e1
caioding <7caioc@gmail.com>
2024-07-29 11:06:32 -0400

    Updating practices codes

---
+++ test.py
@@ -0,0 +1,49 @@
+import numpy as np 
+
+# matriz = [[1,2,3], [4,5,6],[7,8,9]]
+
+# matriz = np.zeros((3,3), type(int))
+# for i in range(3):
+#     for j in range(3):
+#         matriz[i,j] = int(input())
+
+# soma = np.sum(matriz)
+# transposta = np.transpose(matriz)
+# produto_transposta = np.dot(matriz, transposta)
+
+# print(matriz)
+# print()
+# print(transposta)
+# print()
+# print(f'soma = {soma}')
+# print()
+# print(f"Produto\n {produto_transposta}")
+
+# matriz = [[1,2,3], [4,5,6],[7,8,9]]
+# somaDp=0
+# somaDs=0
+# n = len(matriz)
+# for i in range(3):
+#     for j in range(3):
+#         if(i==j):
+#             somaDp += matriz[i][j]
+#         if(j==n -i -1):
+#             somaDs+=matriz[i][j]
+# print(matriz)
+# print(somaDp)
+# print(somaDs)
+# print(somaDp + somaDs)
+
+# Strings
+# nome = input("Digite o seu nome: ")
+# print(f"A string digitada é {nome}")
+# print(f"a string em caixa alta é {nome.upper()}")
+# print(f"a string em em minúsculo é {nome.lower()}")
+# print(f"O tamanho da string é {len(nome)}")
+# print(f"Somente a partir do segundo caracter {nome[1:]}")
+
+# Tuplas
+tupla = (1,2,3,4,5)
+print(tupla[0])
+print(tupla[-1])
+print(tupla[:3])
