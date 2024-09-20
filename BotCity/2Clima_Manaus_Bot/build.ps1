b6e482bed6e1e6fda809dd73d7b08002cddc2785
caioding <7caioc@gmail.com>
2024-09-20 10:50:33 -0400

    update

--- BotCity/2Clima_Manaus_Bot/build.ps1
+++
@@ -1,3 +0,0 @@
-$exclude = @("venv", "2Clima_Manaus_Bot.zip")
-$files = Get-ChildItem -Path . -Exclude $exclude
-Compress-Archive -Path $files -DestinationPath "2Clima_Manaus_Bot.zip" -Force
--- BotCity/2Clima_Manaus_Bot/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-zip -r "2Clima_Manaus_Bot.zip" * -x "2Clima_Manaus_Bot.zip"
