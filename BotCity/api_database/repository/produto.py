b865010961bb45ab7e16fa24e15e8e107610e6a7
caioding <7caioc@gmail.com>
2024-09-23 21:20:47 -0400

    update

--- BotCity/api_database/repository/produto.py
+++ BotCity/api_database/repository/produto.py
@@ -6,7 +6,7 @@ def exist_product(id):
     try:
         conect = database.create_db()
         cursor = conect.cursor()
-        sql = f"SELECT * FROM produto WHERE id = '{id}'"
+        sql = f"SELECT * FROM product WHERE id = '{id}'"
         cursor.execute()
         list_product = cursor.fetchall()
         if len(list_product) == 0:
@@ -22,12 +22,12 @@ def exist_product(id):
     return exist
 
 # Criar produto
-def create_product(produto):
+def create_product(product):
     try:
         # Manipular o banco de dados
         conect = database.create_db()
         cursor = conect.cursor()
-        sql = f"INSERT INTO produto(descricao, unidade, quantidade, preco_real, preco_dolar) VALUES('{produto['descricao']}','{produto['unidade']}', '{produto['quantidade']}', '{produto['preco_real']}','{produto['preco_dolar']}')"
+        sql = f"INSERT INTO product(descricao, unidade, quantidade, preco_real, preco_dolar) VALUES('{product['descricao']}','{product['unidade']}', '{product['quantidade']}', '{product['preco_real']}','{product['preco_dolar']}')"
         print(sql)
         cursor.execute(sql)
         last_id = cursor.lastrowid
@@ -41,13 +41,12 @@ def create_product(produto):
     return last_id 
 
 # Lista de produtos
-
 def list_products():
     products = list()
     try:
         conect = database.create_db()
         cursor = conect.cursor()
-        sql = 'SELECT * FROM produto ORDER BY descricao'
+        sql = 'SELECT * FROM product ORDER BY descricao'
         cursor.execut(sql)
         list_product = cursor.fetchall()
         # Tratar dados para uma estrutura JSON
@@ -55,10 +54,11 @@ def list_products():
             products.append(
                 {
                   'id': product[0],
-                  'nome': product[1],
-                  'login': product[2],
-                  'senha': product[3],
-                  'email': product[4]
+                  'descricao': product[1],
+                  'unidade': product[2],
+                  'quantidade': product[3],
+                  'preco_real': product[4],
+                  'preco_dolar': product[5]
                 }
             )
     except Exception as ex:
@@ -68,3 +68,54 @@ def list_products():
         conect.close()
     
     return products
+
+# Obter produto por id
+def get_product_id(id):
+    products = list()
+    try:
+        conect = database.create_db()
+        cursor = conect.cursor()
+        sql = f"SELECT * FROM product WHERE id = '{id}'"
+        cursor.execute(sql)
+        list_products = cursor.fetchall()
+        # Tratar dados para uma estrutura JSON
+        for product in list_products:
+            product.append(
+                {
+                  'id': product[0],
+                  'descricao': product[1],
+                  'unidade': product[2],
+                  'quantidade': product[3],
+                  'preco_real': product[4],
+                  'preco_dolar': product[5]
+                }
+            )
+    except Exception as ex:
+        print(f'Erro: obter produto pelo id: {ex}')
+    
+    return products
+
+# Atualizar produto
+def update_product(product):
+    try:
+        # Manipular Banco de Dados
+        conect = database.create_db()
+        cursor = conect.cursor()
+        sql = f"UPDATE product SET descricao = '{product['descricao']}', unidade = '{product['unidade']}', quantidade = '{product['quantidade']}', preco_real = '{product['preco_real']}', preco_dolar = '{product['preco_dolar']}' WHERE id = '{product['id']}' "
+        cursor.execute(sql)
+        conect.commit()
+    except Exception as ex:
+        print(f'Erro: Falha na atualização: {ex}')
+
+# Deletar produto
+def delete_product(id):
+    try:
+        # Manipular o banco de dados
+        conect = database.create_db()
+        cursor = conect.cursor()
+        sql = f'DELETE FROM product WHERE id = {id}'
+        cursor.execute(sql)
+        conect.commit()
+    except Exception as ex:
+        print(f'Erro: Falha em deletar produto: {ex}')
+        
